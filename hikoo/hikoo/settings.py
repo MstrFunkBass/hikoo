13a5164f0082b88f5cb121c3264d97c9c2f512cb
aaron grove <aaron_grove@hotmail.co.uk>
2023-07-17 17:12:13 +0100

    replaced secret key and disabled debug

--- hikoo/hikoo/settings.py
+++ hikoo/hikoo/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6%3&*$jj#tvw%r75#k8k+7+jwwy@xe5e^boad=nsxezj==pjj'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
 
 
 # Application definition
