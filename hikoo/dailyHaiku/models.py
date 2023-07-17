06d1f3609b2aff2355a9f228c2cc5e5ddd00803f
aaron grove <aaron_grove@hotmail.co.uk>
2023-07-17 15:52:52 +0100

    created Haiku model

--- hikoo/dailyHaiku/models.py
+++ hikoo/dailyHaiku/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Haiku(models.Model):
+    haiku_id = models.AutoField(primary_key=True)
+    line_one = models.CharField(max_length=100)
+    line_two = models.CharField(max_length=100)
+    line_three = models.CharField(max_length=100)
+    date_created = models.DateTimeField("date published")
+    label = 'haikuEntry'
